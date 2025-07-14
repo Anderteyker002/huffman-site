c4ab93d0f7876aa2aa246dcd9c84b40d9d812f75
002 <zhnetsvoskovatiy@yandex.com>
2025-07-15 01:03:02 +0300

    пятый коммит: полный адаптив index

--- script/top.js
+++ script/top.js
@@ -5,21 +5,39 @@ const a_1 = document.getElementById('a_1')
 
 const div_1 = document.getElementById('div_1')
 const div_2 = document.getElementById('div_2')
+const div_1_1 = document.createElement('p')
+const div_1_2 = document.createElement('a')
+div_1_1.innerHTML = 'Программа, которая сжимает строку, вводимую пользователем, по алгоритму сжатия Хаффмана'
+div_1_2.innerHTML = 'Перейти на сайт'
 
 const w = screen.width
 const h = screen.height
+let flag = true
 
 if (document.readyState == 'loading') {
     if (w <= 1450) {
-        a_1.setAttribute('href', '')
+        flag = false
+        a_1.setAttribute('href', '#123')
+        div_2.style.display = 'none'
+        div_1_1.setAttribute('class', 'text_1_1')
+        div_1_2.setAttribute('class', 'text_1_1')
+        div_1_2.style.textDecoration = 'underline #9a1750'
+        div_1_2.style.fontWeight = '100'
+        div_1_2.setAttribute('href', './base.html')
+    //  div_1_2.style.top = h+'px'
+    //  div_1_1.style.top = h+'px'
+        div_1.appendChild(div_1_1)
+        div_1.appendChild(div_1_2)
     }
 }
 
 
 text_1.addEventListener('mouseover', function(event){
     event.preventDefault()
-    text_1.style.borderTop = 'solid'
-    text_1.style.borderBottom = 'solid'
+    if (flag) {
+        text_1.style.borderTop = 'solid'
+        text_1.style.borderBottom = 'solid'
+    }
 })
 
 text_1.addEventListener('mouseout', function(event){
@@ -30,7 +48,9 @@ text_1.addEventListener('mouseout', function(event){
 
 a_1.addEventListener('mouseover', function(event){
     event.preventDefault()
-    a_1.style.animation = 'None'
+    if (flag) {
+        a_1.style.animation = 'None'
+    }
 })
 
 a_1.addEventListener('mouseout', function(event){
@@ -40,9 +60,11 @@ a_1.addEventListener('mouseout', function(event){
 
 text_1.addEventListener('mouseover', function(event){
     event.preventDefault()
-    for (let i=100; i>70; i--) {
-        div_1.style.width = i+'%'
-        div_2.style.left = i+'%'
+    if (flag) {
+        for (let i=100; i>70; i--) {
+            div_1.style.width = i+'%'
+            div_2.style.left = i+'%'
+        }
     }
 })
 
